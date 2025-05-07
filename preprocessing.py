084317af03302aaf361b95bd2fd16d6a91451c51
nsimonato8 <nsimonato8@gmail.com>
2025-05-07 14:59:26 +0200

    Integrated CLAHE method with existing pipeline

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,13 @@
 import cv2
-from typing import Any
 from concurrent.futures import ThreadPoolExecutor
 from typing import List
 from image import Image
+from preprocessing2 import simple_underwater_correction
+from deprecated import deprecated
 
-def preprocess_image(image: Image) -> Image:
+
+@deprecated(reason="Use simple_underwater_correction instead")
+def _preprocess_image(image: Image) -> Image:
     """
     Performs histogram equalization and radial distortion correction on the input image.
 
@@ -22,14 +25,20 @@ def preprocess_image(image: Image) -> Image:
 
         # Ensure the image is large enough for cropping
         if height < 1500 or width < 1500:
-            raise ValueError("Image dimensions must be at least 1500x1500 for radial distortion correction.")
+            raise ValueError(
+                "Image dimensions must be at least 1500x1500 for radial distortion correction."
+            )
 
         # Crop to the central 1500x1500 pixels
         start_x = (width - 1500) // 2
         start_y = (height - 1500) // 2
-        cropped_image = equalized_image.image[start_y:start_y + 1500, start_x:start_x + 1500]
+        cropped_image = equalized_image.image[
+            start_y : start_y + 1500, start_x : start_x + 1500
+        ]
 
-        image.image = cropped_image  # Update the image attribute with the processed image
+        image.image = (
+            cropped_image  # Update the image attribute with the processed image
+        )
 
         return image
 
@@ -37,7 +46,28 @@ def preprocess_image(image: Image) -> Image:
         raise RuntimeError(f"An error occurred during image preprocessing: {e}")
 
 
-def preprocess_images_parallel(images: List[Image], preprocessing: function=preprocess_image) -> List[Image]:
+def preprocess_image(image: Image) -> Image:
+    """
+    Applies simple underwater correction to the input image.
+
+    Args:
+        image (Image): Input image to be processed.
+
+    Returns:
+        Image: Processed image with simple underwater correction applied.
+    """
+    try:
+        image.image = simple_underwater_correction(
+            image.image
+        )  # Update the image attribute with the processed image
+        return image
+    except Exception as e:
+        raise RuntimeError(f"An error occurred during image preprocessing: {e}")
+
+
+def preprocess_images_parallel(
+    images: List[Image], preprocessing: function = _preprocess_image
+) -> List[Image]:
     """
     Runs the preprocess_image function in parallel using multithreading.
 
