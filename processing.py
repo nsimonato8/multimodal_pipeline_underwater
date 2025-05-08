2c3d8579fa8d756d90761f462011e69f1302224e
nsimonato8bis <nsimonato8.bis@gmail.com>
2025-05-08 15:15:41 +0200

    Update processing.py
    Fixed Image building logic

--- processing.py
+++ processing.py
@@ -24,14 +24,14 @@ def get_largest_bbox_label(workflow_result: Dict[str, Any]) -> Optional[str]:
 
 def detect_and_segmentation_workflow(images: List[Image], prompt: str) -> List[Image]:
     global client
-    result = client.run_workflow(
+    results = client.run_workflow(
         workspace_name=os.getenv("ROBOFLOW_WORKSPACE_NAME", ""),
         workflow_id=os.getenv("ROBOFLOW_DETECTION_WORKFLOW_ID", ""),
         images=list(map(lambda x: x.image, images)),
         parameters={"detection_prompt": prompt}
         )
     
-    return list(map(Image.from_workflow_result, result)) # TODO: Implement parsing logic inside the Image class with a static method.
+    return list(map(lambda res, img: Image.from_workflow_result(res, img), results, images))
 
 
 def frame_selection(images: List[Image]) -> List[Artifact]:
