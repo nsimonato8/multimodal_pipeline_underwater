7af7aa8f22fc0abdd4247738afe70b365198fd5c
Niccolò Simonato <nsimonato8@gmail.com>
2025-05-10 13:12:30 +0200

    Refactor workflow functions to include client parameter and enhance progress tracking with tqdm

--- processing.py
+++ processing.py
@@ -3,6 +3,7 @@ from image import Image
 from artifact import Artifact
 import os
 from itertools import groupby
+import tqdm
 import supervision as sv
 import numpy as np
 
@@ -21,13 +22,15 @@ def get_largest_bbox_label(predictions: Dict[str, Any]) -> Optional[str]:
         return None
 
 
-def detect_and_segmentation_workflow(images: List[Image], prompt: str) -> List[Image]:
-    global client
-    results = client.run_workflow(
-        workspace_name=os.getenv("ROBOFLOW_WORKSPACE_NAME", ""),
-        workflow_id=os.getenv("ROBOFLOW_DETECTION_WORKFLOW_ID", ""),
-        images=list(map(lambda x: x.image, images)),
-        parameters={"detection_prompt": prompt}
+def detect_and_segmentation_workflow(client, images: List[Image], prompt: str) -> List[Image]:
+
+    results = []
+    for image in tqdm.tqdm(images):
+        results += client.run_workflow(
+            workspace_name=os.getenv("ROBOFLOW_WORKSPACE_NAME", ""),
+            workflow_id=os.getenv("ROBOFLOW_DETECTION_WORKFLOW_ID", ""),
+            images={'input_image': image.image}, 
+            parameters={"detection_prompt": prompt}
         )
     
     return list(map(lambda res, img: Image.from_workflow_result(res, img), results, images))
@@ -49,16 +52,17 @@ def frame_selection(images: List[Image]) -> List[Artifact]:
     return [Artifact(name=label, images=list(group)) for label, group in grouped_images if label is not None]
 
 
-def generate_frame_description(artifact: Artifact) -> Artifact:
+def generate_frame_description(client, artifacts: List[Artifact]) -> List[Artifact]:
     # This function adds the caption to the Artifact object.
     def parse_caption(result: Dict[str, Any]) -> str:
         return result.get("model",{}).get("raw_output", "")
         
-    global client
-    result = client.run_workflow(
-        workspace_name=os.getenv("ROBOFLOW_WORKSPACE_NAME", ""),
-        workflow_id=os.getenv("ROBOFLOW_DESCRIPTION_WORKFLOW_ID", ""),
-        images=artifact.images, # TODO: Implement a logic for the selection of the best image among the pictures of the artifact.
-    )
-    artifact.caption = parse_caption(result)
-    return artifact
+    result = []
+    for artifact in tqdm.tqdm(artifacts):
+        print(type(artifact.best_image.image))
+        result += parse_caption(client.run_workflow(
+            workspace_name=os.getenv("ROBOFLOW_WORKSPACE_NAME", ""),
+            workflow_id=os.getenv("ROBOFLOW_DESCRIPTION_WORKFLOW_ID", ""),
+            images={'image': artifact.best_image.image}, # TODO: Implement a logic for the selection of the best image among the pictures of the artifact.
+        ))
+    return result
