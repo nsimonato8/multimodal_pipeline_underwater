c9de39aec81447af6d0ad016f79bb815a0b271a4
Niccolò Simonato <nsimonato8@gmail.com>
2025-05-12 23:38:30 +0200

    Remove deprecated image preprocessing function and update parallel processing to use the main preprocess_image function

--- preprocessing.py
+++ preprocessing.py
@@ -3,47 +3,6 @@ from concurrent.futures import ThreadPoolExecutor
 from typing import List, Callable
 from image import Image
 from preprocessing2 import simple_underwater_correction
-from deprecated import deprecated
-
-
-@deprecated(reason="Use simple_underwater_correction instead")
-def _preprocess_image(image: Image) -> Image:
-    """
-    Performs histogram equalization and radial distortion correction on the input image.
-
-    Args:
-        image (Image): Input image to be processed.
-
-    Returns:
-        Image: Processed image with histogram equalization and radial distortion correction applied.
-    """
-    try:
-        equalized_image = histogram_equalization(image)  # Apply histogram equalization
-
-        # Get image dimensions
-        height, width = equalized_image.image.shape[:2]
-
-        # Ensure the image is large enough for cropping
-        if height < 1500 or width < 1500:
-            raise ValueError(
-                "Image dimensions must be at least 1500x1500 for radial distortion correction."
-            )
-
-        # Crop to the central 1500x1500 pixels
-        start_x = (width - 1500) // 2
-        start_y = (height - 1500) // 2
-        cropped_image = equalized_image.image[
-            start_y : start_y + 1500, start_x : start_x + 1500
-        ]
-
-        image.image = (
-            cropped_image  # Update the image attribute with the processed image
-        )
-
-        return image
-
-    except Exception as e:
-        raise RuntimeError(f"An error occurred during image preprocessing: {e}")
 
 
 def preprocess_image(image: Image) -> Image:
@@ -66,7 +25,7 @@ def preprocess_image(image: Image) -> Image:
 
 
 def preprocess_images_parallel(
-    images: List[Image], preprocessing: Callable = _preprocess_image
+    images: List[Image], preprocessing: Callable = preprocess_image
 ) -> List[Image]:
     """
     Runs the preprocess_image function in parallel using multithreading.
@@ -82,18 +41,3 @@ def preprocess_images_parallel(
         results = executor.map(preprocessing, images)
 
     return list(results)
-
-
-def histogram_equalization(image: Image) -> Image:
-
-    img = image.image
-
-    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-
-    # equalize the histogram of the Y channel
-    img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
-
-    # convert the YUV image back to RGB format
-    image.image = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-
-    return image
