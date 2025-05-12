7d27df859fe4470adcf389020242f77008b71701
Niccolò Simonato <nsimonato8@gmail.com>
2025-05-12 21:41:59 +0200

    Add OpenAI to install_requires

--- setup.py
+++ setup.py
@@ -3,7 +3,17 @@ from setuptools import setup
 setup(
     name="multimodal_pipeline_underwater",
     version="0.0.1",
-    install_requires=["click", "python-dotenv", "inference_sdk", "opencv-python", "deprecated", "numpy", "tqdm", "supervision"],
+    install_requires=[
+        "click",
+        "python-dotenv",
+        "inference_sdk",
+        "opencv-python",
+        "deprecated",
+        "numpy",
+        "tqdm",
+        "supervision",
+        "openai"
+    ],
     entry_points={
         "console_scripts": [
             "pipeline = pipeline:main",
